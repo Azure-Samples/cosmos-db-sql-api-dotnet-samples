9d6609e6934f6b1845025e424c30a03ed8f7276e
Sidney Andrews <sidandrews@microsoft.com>
2022-05-18 09:19:11 -0400

    Reformatted comments

--- src/Program.cs
+++ src/Program.cs
@@ -2,18 +2,20 @@
 // Copyright (c) Microsoft Corporation.  All rights reserved.
 // ------------------------------------------------------------
 
-// <using_directives> Using blocks
+// <using_directives> 
 using Microsoft.Azure.Cosmos;
 // </using_directives>
 
-// <client_credentials> New instance of CosmosClient class
+// <client_credentials> 
+// New instance of CosmosClient class
 using CosmosClient client = new(
     accountEndpoint: Environment.GetEnvironmentVariable("COSMOS_ENDPOINT")!,
     authKeyOrResourceToken: Environment.GetEnvironmentVariable("COSMOS_KEY")!
 );
 // </client_credentials>
 
-// <new_database> Database reference with creation if it does not already exist
+// <new_database> 
+// Database reference with creation if it does not already exist
 Database database = await client.CreateDatabaseIfNotExistsAsync(
     id: "tododatabase"
 );
@@ -21,7 +23,8 @@ Database database = await client.CreateDatabaseIfNotExistsAsync(
 Console.WriteLine($"New database:\t{database.Id}");
 // </new_database>
 
-// <new_container> Container reference with creation if it does not alredy exist
+// <new_container> 
+// Container reference with creation if it does not alredy exist
 Container container = await database.CreateContainerIfNotExistsAsync(
     id: "taskscontainer",
     partitionKeyPath: "/partitionKey",
@@ -31,7 +34,8 @@ Container container = await database.CreateContainerIfNotExistsAsync(
 Console.WriteLine($"New container:\t{container.Id}");
 // </new_container>
 
-// <new_item> Create new object and upsert (create or replace) to container
+// <new_item> 
+// Create new object and upsert (create or replace) to container
 TodoItem newItem = new(
     id: "fb59918b-fb3d-4549-9503-38bee83a6e1d",
     partitionKey: "personal-tasks-user-88033a55",
@@ -48,7 +52,8 @@ TodoItem createdItem = await container.UpsertItemAsync<TodoItem>(
 Console.WriteLine($"Created item:\t{createdItem.id}\t[{createdItem.partitionKey}]");
 // </new_item>
 
-// <query_items> Create query using a SQL string and parameters
+// <query_items> 
+// Create query using a SQL string and parameters
 var query = new QueryDefinition(
     query: "SELECT * FROM todo t WHERE t.partitionKey = @key"
 )
