5941e6ba49bb980bff3f5756e891c4b843288043
seesharprun <sidandrews@microsoft.com>
2022-05-18 09:45:08 -0400

    Updated build script

---
+++ 102-client-connection-string/Program.cs
@@ -0,0 +1,15 @@
+﻿// ------------------------------------------------------------
+// Copyright (c) Microsoft Corporation.  All rights reserved.
+// ------------------------------------------------------------
+
+// <using_directives> 
+using Microsoft.Azure.Cosmos;
+// </using_directives>
+
+// <connection_string> 
+// New instance of CosmosClient class using a connection string
+using CosmosClient client = new(
+    accountEndpoint: Environment.GetEnvironmentVariable("COSMOS_ENDPOINT")!,
+    authKeyOrResourceToken: Environment.GetEnvironmentVariable("COSMOS_KEY")!
+);
+// </connection_string>
